1024f26e653278fd05deeccde3bac249167e3782
maridicesare <marianna.dicesare@correo.unimet.edu.ve>
2024-08-05 10:27:33 -0400

    create starship class

---
+++ starships.py
@@ -0,0 +1,22 @@
+class Starships:
+    starship_list = []
+
+    def __init__(self, name, model, starship_class, manufacturer, cost_in_credits, lenght, crew, passengers, max_atmosphering_speed, hyperdrive_rating, MGLT, cargo_capacity, consumables, pilots, url, created, edited) -> None:
+        self.name = name
+        self.model = model
+        self.starship_class = starship_class
+        self.manufacturer = manufacturer
+        self.cost_in_credits = cost_in_credits
+        self.lenght = lenght
+        self.crew = crew
+        self.passengers = passengers 
+        self.max_atmosphering_speed = max_atmosphering_speed
+        self.hyperdrive_rating = hyperdrive_rating
+        self.MGLT = MGLT
+        self.cargo_capacity = cargo_capacity
+        self.consumables = consumables
+        self.pilots = pilots
+        self.url = url
+        self.created = created
+        self.edited = edited
+        
