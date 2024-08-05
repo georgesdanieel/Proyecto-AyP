f62016fb98ec5c3c89536f2b401fb83dd660d238
Daniel <dgeorges@correo.unimet.edu.ve>
2024-08-05 09:06:24 -0400

    Create planet class

---
+++ planet.py
@@ -0,0 +1,24 @@
+class Planet:
+    planet_list = []  # Lista para almacenar instancias de Planet
+    
+    def __init__(self, name, diameter, rotation_period, orbital_period, gravity, population, climate, terrain, surface_water, url, created, edited):
+        self.name = name
+        self.diameter = diameter
+        self.rotation_period = rotation_period
+        self.orbital_period = orbital_period
+        self.gravity = gravity
+        self.population = population
+        self.climate = climate
+        self.terrain = terrain
+        self.surface_water = surface_water
+        self.url = url  # URL del detalle de la API
+        self.created = created
+        self.edited = edited
+
+    def __repr__(self):
+        return (f"Planet(name={self.name}, diameter={self.diameter}, rotation_period={self.rotation_period}, "
+                f"orbital_period={self.orbital_period}, gravity={self.gravity}, population={self.population}, "
+                f"climate={self.climate}, terrain={self.terrain}, surface_water={self.surface_water}, "
+                f"url={self.url}, created={self.created}, edited={self.edited})")
+
+    
