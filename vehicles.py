e55071994c8f266d55fe5b6a4b94bf9b0444b8f5
maridicesare <marianna.dicesare@correo.unimet.edu.ve>
2024-08-06 15:34:42 -0400

    update vehicles

--- vehicles.py
+++ vehicles.py
@@ -1,7 +1,7 @@
 class Vehicle:
     vehicle_list=[] 
 
-    def __init__(self, name, model, vehicle_class, manufacturer,cost_in_credits, lenght, crew, passengers, max_atmosphering_speed, cargo_capacity, consumables, films, pilots, url, created, edited) -> None:
+    def __init__(self, name, model, vehicle_class, manufacturer, cost_in_credits, lenght, crew, passengers, max_atmosphering_speed, cargo_capacity, consumables, films, pilots, url, created, edited) -> None:
         self.name=name
         self.model=model
         self.vehicle_class=vehicle_class
@@ -21,6 +21,9 @@ class Vehicle:
 
     def __repr__(self):
         return(f"Vehicle(name={self.name}, model={self.model}, vehicle_class={self.vehicle_class}, "
-               f"manufacturer={self.manufacturer}, cost_in_credits={cost_in_credits}, lenght={self.lenght}, "
+               f"manufacturer={self.manufacturer}, cost_in_credits={cost_in_credits}, lenght={self.lenght}, " 
+               f"crew={self.crew}, passengers={self.passengers}, max_atmosphering_speed={self.max_atmosphering_speed}, "
+               f"cargo_capacity={self.cargo_capacity}, consumables={self.consumables}, films={self.films}, "
+               f"pilots={self.pilots}, url={self.url}, created={self.created}, edited={self.edited})")
 
 
