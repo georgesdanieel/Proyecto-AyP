6d6b3a1f9bda0185d05a0957be0a507581f45870
Daniel <dgeorges@correo.unimet.edu.ve>
2024-08-07 12:17:41 -0400

    import (Main pt1)

---
+++ main.py
@@ -0,0 +1,12 @@
+import requests
+import json
+from film import Film 
+from character import Character
+from planet import Planet
+from species import Species
+from vehicle import Vehicle
+from starship import Starship
+from stats import *
+from missions import *
+
+
