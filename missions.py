5741c539c9d417c4b7d26afaaf5ff8dc11c46a28
maridicesare <marianna.dicesare@correo.unimet.edu.ve>
2024-08-10 15:36:41 -0400

    update missions

--- missions.py
+++ missions.py
@@ -25,4 +25,35 @@ def seleccionar_opcion(opciones, max_seleccion=1, permitir_repetidos=True):
             print("Opción inválida. Por favor, elija una opción válida.")
     return seleccionados
 
+def contruir_mision():
+    if len(missions)>=5:
+        print("No se pueden definir más de 5 misiones")
+        return
+    
+    mision={}
+    
+    #Solicitar el nombre de la misión
+    mision["nombre"]= input("Nombre de la misión: ")
 
+    #Solicitar el planeta destino 
+    print("Seleccione el planeta destino:")
+    planetas=[planet.name for planet in Planet.planet_list]
+    mision["planeta_destino"]=seleccionar_opcion(planetas, max_seleccion=1)[0]
+
+    #Solicitar la nave a utilizar
+    print("Seleccione la nave a utilizar:")
+    naves=[starship.name for starship in Starship.starship_list]
+    mision["nave"]=seleccionar_opcion(naves, max_seleccion=1)[0]
+
+    #Solicitar las armas a utilizar 
+    print("Seleccione hasta 7 armas:")
+    mision["armas"]=seleccionar_opcion(armas_disponibles, max_seleccion=7)
+
+    #Solicitar los integrantes de la misión
+    print("Seleccione hasta 7 integrantes:")
+    integrantes=[character.name for character in Character.character_list]
+    mision["integrantes"]=seleccionar_opcion(integrantes, max_seleccion=7, permitir_repetido=False)
+
+    #Añadir la misión a la lista de misiones
+    missions.append(mision)
+    print(f"Misión '{mision['nombre']}' construida exitosamente.")
