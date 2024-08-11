6965a4d82f436a8bbcdf9ae4277e68d9620a72ee
vizcarrondov <vizcarrondov@correo.unimet.edu.ve>
2024-08-11 17:11:33 -0400

    Add files via upload

---
+++ stats.py
@@ -0,0 +1,51 @@
+import pandas as pd
+import numpy as np
+
+file=('/Users/valentinavizcarrondo/Downloads/Proyecto-AyP-main-2/starships.csv')
+df = pd.read_csv(file)
+
+parametros=['MGLT','hyperdrive_rating','cost_in_credits','max_atmosphering_speed']
+
+clasificacion= {}
+
+for parametro in parametros:
+    clase=df.groupby('starship_class')[parametro].agg(['count','mean', 'max', 'min'])
+    moda=df.groupby('starship_class')[parametro].agg(lambda x: x.value_counts().index[0] if len(x.value_counts()) > 0 else np.nan)
+    clase['mode']=moda
+    clasificacion[parametro]=clase
+
+def show_stats():
+    opciones={
+
+    '1':'MGLT',
+    '2':'hyperdrive_rating',
+    '3':'cost_in_credits',
+    '4':'max_atmosphering_speed'
+
+    }
+
+    while True:
+        print('\n Seleccione una opcion para ver sus estadisticas. Coloca cualquier letra para salir')
+        for key, value in opciones.items():
+            print(f'{key}. {value}')
+        opcion_seleccionada=input('--->  ')
+
+        if opcion_seleccionada=='1':
+            print('Estadisticas de los tipos de naves segun MGLT')
+            print(clasificacion['MGLT'])
+        elif opcion_seleccionada=='2':
+            print('Estadisticas de los tipos de naves segun hiperimpulsor')
+            print(clasificacion['hyperdrive_rating'])
+        elif opcion_seleccionada=='3':
+            print('Estadisticas de los tipos de naves segun su costo')
+            print(clasificacion['cost_in_credits'])
+        elif opcion_seleccionada=='4':
+            print('Estadisticas de los tipos de naves segun su maxima velocidad atmósferico')
+            print(clasificacion['max_atmosphering_speed'])
+        else:
+            print('Has salido con exito')
+            break
+
+
+
+
