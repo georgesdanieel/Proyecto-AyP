d132083f7cee3595cf29fb4cacdcd3a92d849329
vizcarrondov <vizcarrondov@correo.unimet.edu.ve>
2024-08-12 01:49:34 -0400

    Update planet_stats.py

--- planet_stats.py
+++ planet_stats.py
@@ -7,18 +7,18 @@ df= pd.read_csv(file)
 planets=df['name']
 characters = []
 
-def planet_graphs():
 
-    for resident in df['residents']:
-        characters.append(resident) 
 
-    plt.barh(planets,characters, color='red') #quiero q se me separe cada 1 unidad
+for resident in df['residents']:
+    characters.append(len(resident.split(',')))
 
-    plt.xticks(range(0,5,1))
-    plt.xlabel('Número de personajes')
-    plt.ylabel('Planetas')
+plt.barh(planets,characters, color='red') #quiero q se me separe cada 1 unidad
 
-    plt.title('Número de personajes por planeta')
+plt.xticks(range(0,5,1))
+plt.xlabel('Número de personajes')
+plt.ylabel('Planetas')
 
-    plt.show()
+plt.title('Número de personajes por planeta')
+
+plt.show()
 
