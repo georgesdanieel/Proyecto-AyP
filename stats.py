f5a71b6a6b3c94697a90700d2106ca7ac005b2e1
vizcarrondov <vizcarrondov@correo.unimet.edu.ve>
2024-08-12 19:36:11 -0400

    Update stats.py

--- stats.py
+++ stats.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
-def plot_characters_per_planet(character_list):
+def personaje_por_mundo(character_list):
    
     for character in character_list:
         planet_names = character.homeworld
@@ -17,7 +17,7 @@ def plot_characters_per_planet(character_list):
     plt.ylabel('Cantidad de personajes')
     plt.show()
 
-def plot_starship_characteristics(starship_list):
+def characteristicas_naves(starship_list):
 
     data = {
         "Name": [],
@@ -34,5 +34,32 @@ def plot_starship_characteristics(starship_list):
         data["MGLT"].append(float(starship.MGLT))
 
     df = pd.DataFrame(data)
-    
     fig, axs = plt.subplots(2, 2, figsize=(14, 10))
+
+    #Length
+    axs[0, 0].bar(df["Name"], df["Length"], color='blue')
+    axs[0, 0].set_title('Longitud de las Naves')
+    axs[0, 0].set_xlabel('Nave')
+    axs[0, 0].set_ylabel('Longitud')
+
+    #Cargo
+    axs[0, 1].bar(df["Name"], df["Cargo Capacity"], color='red')
+    axs[0, 1].set_title('Capacidad de Carga de las Naves')
+    axs[0, 1].set_xlabel('Nave')
+    axs[0, 1].set_ylabel('Capacidad de Carga por nave')
+
+    # Hyperdrive Rating
+    axs[1, 0].bar(df["Name"], df["Hyperdrive Rating"], color='green')
+    axs[1, 0].set_title('Clasificación de Hiperimpulsor de las Naves')
+    axs[1, 0].set_xlabel('Nave')
+    axs[1, 0].set_ylabel('Clasificación de Hiperimpulsor por nave')
+
+    # MGLT
+    axs[1, 1].bar(df["Name"], df["MGLT"], color='purple')
+    axs[1, 1].set_title('MGLT de las Naves')
+    axs[1, 1].set_xlabel('Nave')
+    axs[1, 1].set_ylabel('MGLT')
+
+    plt.tight_layout()
+    plt.show()
+
