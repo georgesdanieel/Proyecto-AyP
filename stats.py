88f2ef36e8f2d54c2b283f72d8e0a4923a996b9b
vizcarrondov <vizcarrondov@correo.unimet.edu.ve>
2024-08-12 19:27:30 -0400

    Update stats.py

--- stats.py
+++ stats.py
@@ -2,22 +2,37 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 def plot_characters_per_planet(character_list):
-    # Collect data
-    planet_names = [character.homeworld for character in character_list]
-
-    # Create DataFrame
+   
+    for character in character_list:
+        planet_names = character.homeworld
+   
     df = pd.DataFrame(planet_names, columns=['Planet'])
     
-    # Count the number of characters per planet
     planet_counts = df['Planet'].value_counts()
 
-    # Generate the plot
     plt.figure(figsize=(10, 6))
-    planet_counts.plot(kind='bar', color='skyblue')
+    planet_counts.plot(kind='bar', color='red')
     plt.title('Cantidad de personajes nacidos en cada planeta')
     plt.xlabel('Planeta')
     plt.ylabel('Cantidad de personajes')
-    plt.xticks(rotation=45, ha='right')
-    plt.tight_layout()
     plt.show()
 
+def plot_starship_characteristics(starship_list):
+
+    data = {
+        "Name": [],
+        "Length": [],
+        "Cargo Capacity": [],
+        "Hyperdrive Rating": [],
+        "MGLT": []
+    }
+    for starship in starship_list:
+        data["Name"].append(starship.name)
+        data["Length"].append(float(starship.length))
+        data["Cargo Capacity"].append(float(starship.cargo_capacity))
+        data["Hyperdrive Rating"].append(float(starship.hyperdrive_rating))
+        data["MGLT"].append(float(starship.MGLT))
+
+    df = pd.DataFrame(data)
+    
+    fig, axs = plt.subplots(2, 2, figsize=(14, 10))
