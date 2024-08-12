747536e0780b310f01d026ef7abff7bbbc385b1c
vizcarrondov <vizcarrondov@correo.unimet.edu.ve>
2024-08-12 19:42:11 -0400

    Update stats.py

--- stats.py
+++ stats.py
@@ -1,17 +1,15 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
-def personaje_por_mundo(character_list):
-   
-    for character in character_list:
-        planet_names = character.homeworld
-   
-    df = pd.DataFrame(planet_names, columns=['Planet'])
+def personaje_por_mundo():
+
+    file= '/Users/valentinavizcarrondo/Downloads/Proyecto-AyP-main/csv/planets.csv'
+    df= pd.read_csv(file)
     
     planet_counts = df['Planet'].value_counts()
 
     plt.figure(figsize=(10, 6))
-    planet_counts.plot(kind='bar', color='red')
+    planet_counts.plot(kind='barh', color='red')
     plt.title('Cantidad de personajes nacidos en cada planeta')
     plt.xlabel('Planeta')
     plt.ylabel('Cantidad de personajes')
@@ -63,3 +61,47 @@ def characteristicas_naves(starship_list):
     plt.tight_layout()
     plt.show()
 
+def stats_generales():
+    file=('/Users/valentinavizcarrondo/Downloads/Proyecto-AyP-main-2/starships.csv')
+    df = pd.read_csv(file)
+
+    parametros=['MGLT','hyperdrive_rating','cost_in_credits','max_atmosphering_speed']
+
+    clasificacion= {}
+
+    for parametro in parametros:
+        clase=df.groupby('starship_class')[parametro].agg(['count','mean', 'max', 'min'])
+        moda=df.groupby('starship_class')[parametro].agg(lambda x: x.value_counts().index[0] if len(x.value_counts()) > 0 else np.nan)
+        clase['mode']=moda
+        clasificacion[parametro]=clase
+
+    opciones={
+
+    '1':'MGLT',
+    '2':'hyperdrive_rating',
+    '3':'cost_in_credits',
+    '4':'max_atmosphering_speed'
+
+    }
+
+    while True:
+        print('\n Seleccione una opcion para ver sus estadisticas. Coloca cualquier letra para salir')
+        for key, value in opciones.items():
+            print(f'{key}. {value}')
+        opcion_seleccionada=input('--->  ')
+
+        if opcion_seleccionada=='1':
+            print('Estadisticas de los tipos de naves segun MGLT')
+            print(clasificacion['MGLT'])
+        elif opcion_seleccionada=='2':
+            print('Estadisticas de los tipos de naves segun hiperimpulsor')
+            print(clasificacion['hyperdrive_rating'])
+        elif opcion_seleccionada=='3':
+            print('Estadisticas de los tipos de naves segun su costo')
+            print(clasificacion['cost_in_credits'])
+        elif opcion_seleccionada=='4':
+            print('Estadisticas de los tipos de naves segun su maxima velocidad atmósferico')
+            print(clasificacion['max_atmosphering_speed'])
+        else:
+            print('Has salido con exito')
+            break
