c2cfbc950ac54f2307cf41288d2c19f1528037a6
vizcarrondov <vizcarrondov@correo.unimet.edu.ve>
2024-08-12 02:33:20 -0400

    Update starship_stats.py

--- starship_stats.py
+++ starship_stats.py
@@ -22,15 +22,36 @@ def spaceship_graphs():
         for key, value in menu.items():
             print(f'{key}- {value}')
         opcion_seleccionada=input('--->  ')
+
         if opcion_seleccionada=='1':
-            plt.figure(figsize=(14,9))
-            plt.barh(clase, longitud, color='green')
-
+            clase_longitud=df.groupby('starship_class')['length'].mean()
+            plt.barh(clase_longitud.index, clase_longitud.values, color='green')
             plt.xticks(range(0,20000,1000))
-            plt.ylabel('Nombre de la nave')
+            plt.ylabel('Clase de nave')
             plt.xlabel('Longitud')
-            plt.title('Comparacion de la longitud de cada nave')
+            plt.title('Comparacion de la longitud por nave')
 
-spaceship_graphs()
+        elif opcion_seleccionada=='2':
+            clase_cargo=df.groupby('starship_class')['cargo_capacity'].mean()
+            plt.barh(clase_cargo.index, clase_cargo.values, color='green')
+            plt.xticks(range(0,100000000,100000))
+            plt.ylabel('Clase de nave')
+            plt.xlabel('Capacidad de carga')
+            plt.title('Comparacion de la capacidad de carga por nave')
 
+        elif opcion_seleccionada=='3':
+            clase_hiperimpulsor=df.groupby('starship_class')['hyperdrive_rating'].mean()
+            plt.barh(clase_hiperimpulsor.index, clase_hiperimpulsor.values, color='purple')
+            plt.xticks(range(0,10,1))
+            plt.ylabel('Clase de nave')
+            plt.xlabel('Hiperimpulsor')
+            plt.title('Comparacion del hiperimpulsor por nave')
+            
+        elif opcion_seleccionada=='4':
+            clase_mglt=df.groupby('starship_class')['MGLT'].mean()
+            plt.barh(clase_mglt.index, clase_mglt.values, color='orange')
+            plt.xticks(range(0,150,10))
+            plt.ylabel('Clase de nave')
+            plt.xlabel('MGLT')
+            plt.title('Comparacion de MGLT por nave')
 
