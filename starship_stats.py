c22ad344ef0924d23e4f74a8251ca917cfabbd48
vizcarrondov <vizcarrondov@correo.unimet.edu.ve>
2024-08-12 02:12:54 -0400

    Update starship_stats.py

--- starship_stats.py
+++ starship_stats.py
@@ -4,23 +4,28 @@ import pandas as pd
 file='/Users/valentinavizcarrondo/Downloads/Proyecto-AyP-main/csv/starships.csv'
 df=pd.read_csv(file)
 
-nave=df['name']
 longitud=df['length']
 cargo=df['cargo_capacity']
 hiperimpulsor=df['hyperdrive_rating']
 mglt=df['MGLT']
 
+clase=df.groupby['starship_class']
 
 def spaceship_graphs():
-    menu=input('''Selecciona por cual parametro deseas comparar las naves:
-    1. Longitud
-    2. Capacidad de carga
-    3. Hiperimpulsor
-    4. MGLT
-    ''')
+    menu={
+        '1':'Longitud',
+        '2':'Capacidad de carga',
+        '3':'Hiperimpulsor',
+        '4':'MGLT'
+}
+    while True:
+        print('\n Seleccione una opcion para ver sus estadisticas. Coloca cualquier letra para salir')
+        for key, value in menu.items():
+            print(f'{key}- {value}')
+        opcion_seleccionada=input('--->  ')
 
 plt.figure(figsize=(14,9))
-plt.barh(nave, longitud, color='green')
+plt.barh(clase, longitud, color='green')
 
 plt.xticks(range(0,20000,1000))
 plt.ylabel('Nombre de la nave')
@@ -31,7 +36,7 @@ plt.title('Comparacion de la longitud de cada nave')
 plt.show()
 
 plt.figure(figsize=(14,9))
-plt.barh(nave, cargo, color='blue')
+plt.barh(clase, cargo, color='blue')
 
 plt.ylabel('Nombre de la nave')
 plt.xlabel('Cargo')
@@ -44,7 +49,7 @@ plt.show()
 
 plt.figure(figsize=(14,9))
 
-plt.barh(nave, mglt, color='purple')
+plt.barh(clase, mglt, color='purple')
 plt.xticks(range(0,150,10))
 
 plt.ylabel('Nombre de la nave')
