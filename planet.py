f633d5a1110d23914e299a766e0b8d2e43caa24a
Daniel <dgeorges@correo.unimet.edu.ve>
2024-08-11 22:46:18 -0400

    Create planet class pt3

--- planet.py
+++ planet.py
@@ -1,22 +1,26 @@
-class Film:
-    film_list = []
-    def __init__(self, title, episode_id, opening_crawl, director, producer, release_date, species, starships, vehicles, characters, planets, url, created, edited):
-        self.title = title
-        self.episode_id = episode_id
-        self.opening_crawl = opening_crawl
-        self.director = director
-        self.producer = producer
-        self.release_date = release_date
-        self.species = species
-        self.starships = starships
-        self.vehicles = vehicles
-        self.characters = characters
-        self.planets = planets
-        self.url = url
+class Planet:
+    planet_list = []  # Lista para almacenar instancias de Planet
+    
+    def __init__(self, name, diameter, rotation_period, orbital_period, gravity, population, climate, terrain, surface_water, url, created, edited, residents, films):
+        self.name = name
+        self.diameter = diameter
+        self.rotation_period = rotation_period
+        self.orbital_period = orbital_period
+        self.gravity = gravity
+        self.population = population
+        self.climate = climate
+        self.terrain = terrain
+        self.surface_water = surface_water
+        self.url = url  # URL del detalle de la API
         self.created = created
         self.edited = edited
+        self.residents = residents
+        self.films = films
 
     def __repr__(self):
-        return f"Film(title={self.title}, episode_id={self.episode_id}, opening_crawl={self.opening_crawl}, director={self.director}, producer={self.producer}, release_date={self.release_date}, species={self.species}, starships={self.starships}, vehicles={self.vehicles}, characters={self.characters}, planets={self.planets}, url={self.url}, created={self.created}, edited={self.edited})"
+        return (f"Planet(name={self.name}, diameter={self.diameter}, rotation_period={self.rotation_period}, "
+                f"orbital_period={self.orbital_period}, gravity={self.gravity}, population={self.population}, "
+                f"climate={self.climate}, terrain={self.terrain}, surface_water={self.surface_water}, "
+                f"url={self.url}, created={self.created}, edited={self.edited})")
+
     
-   
