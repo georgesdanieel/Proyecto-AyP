7a8e0ddb5d8b9192a95841826a1403105b33c0ca
vizcarrondov <vizcarrondov@correo.unimet.edu.ve>
2024-08-12 23:39:57 -0400

    Update starship_stats.py

--- starship_stats.py
+++ starship_stats.py
@@ -20,34 +20,38 @@ def caracteristicas_naves(starship_list):
         
     df = pd.DataFrame(data) #otra forma de recolectar la info para hacer un grafico
     fig, axs = plt.subplots(2, 2, figsize=(14, 10)) 
-
-    axs[0, 0].bar(df["Name"], df["Length"], color='blue')
+    
+ # Length Plot
+    axs[0, 0].bar(df["Name"], df["Length"], color='skyblue')
     axs[0, 0].set_title('Longitud de las Naves')
     axs[0, 0].set_xlabel('Nave')
     axs[0, 0].set_ylabel('Longitud')
-    axs[0, 0].set_yticks(np.arange(0, df["Length"].max(), 100)) #me permite organizar los valores de menor a mayor
-    axs[0, 0].tick_params(axis='x', rotation=90) #hace que los nombres se vean en vertical
+    axs[0, 0].tick_params(axis='x', rotation=90)
 
-    axs[0, 1].bar(df["Name"], df["Cargo Capacity"], color='red')
+    # Cargo Capacity Plot
+    axs[0, 1].bar(df["Name"], df["Cargo Capacity"], color='salmon')
     axs[0, 1].set_title('Capacidad de Carga de las Naves')
     axs[0, 1].set_xlabel('Nave')
-    axs[0, 1].set_ylabel('Capacidad de Carga por nave')
-    axs[0, 1].set_yticks(np.arange(0, df["Cargo Capacity"].max(), 1000))
+    axs[0, 1].set_ylabel('Capacidad de Carga')
     axs[0, 1].tick_params(axis='x', rotation=90)
 
-    axs[1, 0].bar(df["Name"], df["Hyperdrive Rating"], color='green')
+    # Hyperdrive Rating Plot
+    axs[1, 0].bar(df["Name"], df["Hyperdrive Rating"], color='lightgreen')
     axs[1, 0].set_title('Clasificación de Hiperimpulsor de las Naves')
     axs[1, 0].set_xlabel('Nave')
-    axs[1, 0].set_ylabel('Clasificación de Hiperimpulsor por nave')
-    axs[1, 0].set_yticks(np.arange(0, df["Hyperdrive Rating"].max(), 0.5))
+    axs[1, 0].set_ylabel('Clasificación de Hiperimpulsor')
     axs[1, 0].tick_params(axis='x', rotation=90)
 
+    # MGLT Plot
     axs[1, 1].bar(df["Name"], df["MGLT"], color='purple')
     axs[1, 1].set_title('MGLT de las Naves')
     axs[1, 1].set_xlabel('Nave')
     axs[1, 1].set_ylabel('MGLT')
-    axs[1, 1].set_yticks(np.arange(0, df["MGLT"].max(), 10))
     axs[1, 1].tick_params(axis='x', rotation=90)
+
+    plt.tight_layout()
+    plt.show()
+    
 
     plt.tight_layout()
     plt.show()
