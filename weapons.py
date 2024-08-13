527c9fe20e91a3997c84e708b4db20162a973a59
vizcarrondov <vizcarrondov@correo.unimet.edu.ve>
2024-08-12 23:58:11 -0400

    Update weapons.py

--- weapons.py
+++ weapons.py
@@ -10,13 +10,4 @@ class Weapon:
         self.weapon_type = weapon_type
         self.description = description
         self.films = films
-    def __repr__(self):
-        return (f"ID: {self.weapon_id}\n"
-                f"Name: {self.name}\n"
-                f"Model: {self.model}\n"
-                f"Manufacturer: {self.manufacturer}\n"
-                f"Cost in Credits: {self.cost_in_credits}\n"
-                f"Length: {self.length}\n"
-                f"Type: {self.weapon_type}\n"
-                f"Description: {self.description}\n"
-                f"Films: {self.films}\n")
+    
