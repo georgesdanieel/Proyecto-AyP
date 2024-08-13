bcdd071142a66b43c3f8dfe507b06f56cee01ce8
vizcarrondov <vizcarrondov@correo.unimet.edu.ve>
2024-08-12 22:05:45 -0400

    Update starship_stats.py

--- starship_stats.py
+++ starship_stats.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
 
 def caracteristicas_naves(starship_list):
 
@@ -24,24 +25,28 @@ def caracteristicas_naves(starship_list):
     axs[0, 0].set_title('Longitud de las Naves')
     axs[0, 0].set_xlabel('Nave')
     axs[0, 0].set_ylabel('Longitud')
-    axs[0, 0].tick_params(axis='x', rotation=90)
+    axs[0, 0].set_yticks(np.arange(0, df["Length"].max() * 1.1, 100)) #me permite organizar los valores de menor a mayor
+    axs[0, 0].tick_params(axis='x', rotation=90) #hace que los nombres se vean en vertical
 
     axs[0, 1].bar(df["Name"], df["Cargo Capacity"], color='red')
     axs[0, 1].set_title('Capacidad de Carga de las Naves')
     axs[0, 1].set_xlabel('Nave')
     axs[0, 1].set_ylabel('Capacidad de Carga por nave')
+    axs[0, 1].set_yticks(np.arange(0, df["Cargo Capacity"].max() * 1.1, 1000))
     axs[0, 1].tick_params(axis='x', rotation=90)
 
     axs[1, 0].bar(df["Name"], df["Hyperdrive Rating"], color='green')
     axs[1, 0].set_title('Clasificación de Hiperimpulsor de las Naves')
     axs[1, 0].set_xlabel('Nave')
     axs[1, 0].set_ylabel('Clasificación de Hiperimpulsor por nave')
+    axs[1, 0].set_yticks(np.arange(0, df["Hyperdrive Rating"].max() * 1.1, 0.5))
     axs[1, 0].tick_params(axis='x', rotation=90)
 
     axs[1, 1].bar(df["Name"], df["MGLT"], color='purple')
     axs[1, 1].set_title('MGLT de las Naves')
     axs[1, 1].set_xlabel('Nave')
     axs[1, 1].set_ylabel('MGLT')
+    axs[1, 1].set_yticks(np.arange(0, df["MGLT"].max() * 1.1, 10))
     axs[1, 1].tick_params(axis='x', rotation=90)
 
     plt.tight_layout()
