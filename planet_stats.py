af5e7da27baef6122079959e56060831f4275bc1
vizcarrondov <vizcarrondov@correo.unimet.edu.ve>
2024-08-12 20:43:04 -0400

    Update planet_stats.py

--- planet_stats.py
+++ planet_stats.py
@@ -4,15 +4,17 @@ import pandas as pd
 file= '/Users/valentinavizcarrondo/Documents/GitHub/Proyecto-AyP/Proyecto-AyP/Proyecto-AyP/csv/characters.csv'
 df= pd.read_csv(file)
 
-planets=df['homeworld'].value_counts().index
-characters =df['homeworld'].value_counts().values
+def planet_graphs():
+    planets=df['homeworld'].value_counts().index
+    characters =df['homeworld'].value_counts().values
 
-plt.barh(range(len(planets)),characters, color='red') #quiero q se me separe cada 1 unidad
-plt.yticks(range(len(planets)), planets)
+    plt.figure(figsize=(10,8))
+    plt.barh(range(len(planets)),characters, color='red') #quiero q se me separe cada 1 unidad
+    plt.yticks(range(len(planets)), planets)
 
-plt.xlabel('Número de personajes')
-plt.ylabel('Planetas')
-plt.title('Número de personajes por planeta')
+    plt.xlabel('Número de personajes')
+    plt.ylabel('Planetas')
+    plt.title('Número de personajes por planeta')
 
-plt.show()
+    plt.show()
 
