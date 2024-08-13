4f754a83068e26bd587e3c962d90b6fce28dd853
vizcarrondov <vizcarrondov@correo.unimet.edu.ve>
2024-08-12 23:56:45 -0400

    Update character.py

--- character.py
+++ character.py
@@ -14,13 +14,4 @@ class Character:
         self.url = url  # URL del detalle de la API
         self.weight = weight
 
-
-
-    def __repr__(self):
-        return (f"Character(name={self.name}, birth_year={self.birth_year}, eye_color={self.eye_color}, "
-                f"gender={self.gender}, hair_color={self.hair_color}, height={self.height}, "
-                f"mass={self.mass}, skin_color={self.skin_color}, homeworld={self.homeworld}, "
-                f"url={self.url}, created={self.created}, edited={self.edited})")
-
-  
   
