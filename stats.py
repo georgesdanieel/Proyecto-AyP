d2dc583dfd4865bf46ed6a706c9a52e589c07fa6
vizcarrondov <vizcarrondov@correo.unimet.edu.ve>
2024-08-12 20:45:25 -0400

    Update stats.py

--- stats.py
+++ stats.py
@@ -2,67 +2,6 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 
-def personaje_por_mundo(character_list):
-    for character in character_list:
-        planet_names = character.homeworld
-
-    df = pd.DataFrame(planet_names, columns=['Planet'])
-    
-    planet_counts = df['Planet'].value_counts()
-
-    plt.figure(figsize=(10, 6))
-    planet_counts.plot(kind='barh', color='brown')
-    plt.title('Cantidad de personajes nacidos en cada planeta')
-    plt.xlabel('Planeta')
-    plt.ylabel('Cantidad de personajes')
-    plt.show()
-
-def caracteristicas_naves(starship_list):
-
-    data = {
-        "Name": [],
-        "Length": [],
-        "Cargo Capacity": [],
-        "Hyperdrive Rating": [],
-        "MGLT": []
-    }
-    for starship in starship_list:
-        data["Name"].append(starship.name)
-        data["Length"].append(float(starship.length))
-        data["Cargo Capacity"].append(float(starship.cargo_capacity))
-        data["Hyperdrive Rating"].append(float(starship.hyperdrive_rating))
-        data["MGLT"].append(float(starship.MGLT))
-
-    df = pd.DataFrame(data)
-    fig, axs = plt.subplots(2, 2, figsize=(14, 10))
-
-    #Length
-    axs[0, 0].bar(df["Name"], df["Length"], color='blue')
-    axs[0, 0].set_title('Longitud de las Naves')
-    axs[0, 0].set_xlabel('Nave')
-    axs[0, 0].set_ylabel('Longitud')
-
-    #Cargo
-    axs[0, 1].bar(df["Name"], df["Cargo Capacity"], color='red')
-    axs[0, 1].set_title('Capacidad de Carga de las Naves')
-    axs[0, 1].set_xlabel('Nave')
-    axs[0, 1].set_ylabel('Capacidad de Carga por nave')
-
-    # Hyperdrive Rating
-    axs[1, 0].bar(df["Name"], df["Hyperdrive Rating"], color='green')
-    axs[1, 0].set_title('Clasificación de Hiperimpulsor de las Naves')
-    axs[1, 0].set_xlabel('Nave')
-    axs[1, 0].set_ylabel('Clasificación de Hiperimpulsor por nave')
-
-    # MGLT
-    axs[1, 1].bar(df["Name"], df["MGLT"], color='purple')
-    axs[1, 1].set_title('MGLT de las Naves')
-    axs[1, 1].set_xlabel('Nave')
-    axs[1, 1].set_ylabel('MGLT')
-
-    plt.tight_layout()
-    plt.show()
-
 def stats_generales():
     file=('/Users/valentinavizcarrondo/Downloads/Proyecto-AyP-main-2/starships.csv')
     df = pd.read_csv(file)
