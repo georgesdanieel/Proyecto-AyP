dd8434b9b72b1672a702c36e8ea30de89c4633f1
vizcarrondov <vizcarrondov@correo.unimet.edu.ve>
2024-08-12 21:14:52 -0400

    Update missions.py

--- missions.py
+++ missions.py
@@ -11,22 +11,12 @@ def listar_opciones(opciones):
     for i, opcion in enumerate(opciones, 1):
         print(f"{i}. {opcion}")
 
-def seleccionar_opcion(opciones, max_seleccion=1, permitir_repetidos=True):
-    seleccionados=[]
-    while len(seleccionados)<max_seleccion:
-        listar_opciones(opciones)
-        seleccion=input(f"Seleccione una opción (1-{len(opciones)}), o presione Enter para terminar: ")
-        if seleccion=="":
-            break
-        if seleccion.isdigit() and 1 <= int(seleccion) <= len(opciones):
-            eleccion=opciones[int(seleccion) - 1]
-            if not permitir_repetidos and eleccion in seleccionados:
-                print("Esa opción ya ha sido seleccionada. Por favor, elija otra opción.")
-            else:
-                seleccionados.append(eleccion)
-        else: 
-            print("Opción inválida. Por favor, elija una opción válida.")
-    return seleccionados
+def seleccionar_opcion(options, max_seleccion=1):
+    for i, option in enumerate(options, start=1):
+        print(f"{i}. {option}")
+    seleccion = input("Seleccione una opción: ")
+    seleccion = int(seleccion) - 1
+    return [options[seleccion]]
 
 def construir_mision():
     if len(missions)>=5:
@@ -35,30 +25,24 @@ def construir_mision():
     
     mision={} 
     
-    #Solicitar el nombre de la misión
     mision["nombre"]= input("Nombre de la misión: ")
 
-    #Solicitar el planeta destino 
     print("Seleccione el planeta destino:")
     planetas=[planet.name for planet in Planet.planet_list]
     mision["planeta_destino"]=seleccionar_opcion(planetas, max_seleccion=1)[0]
 
-    #Solicitar la nave a utilizar
     print("Seleccione la nave a utilizar:")
     naves=[starship.name for starship in Starship.starship_list]
     mision["nave"]=seleccionar_opcion(naves, max_seleccion=1)[0]
 
-    #Solicitar las armas a utilizar 
     print("Seleccione hasta 7 armas:")
     armas =[weapon.name for weapon in Weapon.weapon_list]
     mision["armas"]=seleccionar_opcion(armas, max_seleccion=7)
 
-    #Solicitar los integrantes de la misión
     print("Seleccione hasta 7 integrantes:")
     integrantes=[character.name for character in Character.character_list]
     mision["integrantes"]=seleccionar_opcion(integrantes, max_seleccion=7, permitir_repetidos=False)
 
-    #Añadir la misión a la lista de misiones
     missions.append(mision)
     print(f"Misión '{mision['nombre']}' construida exitosamente.")
 
