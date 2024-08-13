490c3d813a87a4de5bf977ef12270057460cc4e4
vizcarrondov <vizcarrondov@correo.unimet.edu.ve>
2024-08-12 21:01:11 -0400

    Update starship_stats.py

--- starship_stats.py
+++ starship_stats.py
@@ -12,49 +12,37 @@ def caracteristicas_naves(starship_list):
     }
     for starship in starship_list:
         data["Name"].append(starship.name)
-        if starship.length:
-            data["Length"].append(float(starship.length))
-        else:
-            0
-        if starship.cargo_capacity:
-            data["Cargo Capacity"].append(float(starship.cargo_capacity))
-        else:
-            0
-        if starship.hyperdrive_rating:
-            data["Hyperdrive Rating"].append(float(starship.hyperdrive_rating))
-        else:
-            0
-        if starship.MGLT:
-            data["MGLT"].append(float(starship.MGLT))
-        else:
-            0
+        data["Length"].append(starship.length)
+        data["Cargo Capacity"].append(starship.cargo_capacity)
+        data["Hyperdrive Rating"].append(starship.hyperdrive_rating)
+        data["MGLT"].append(starship.MGLT)
 
     df = pd.DataFrame(data)
     fig, axs = plt.subplots(2, 2, figsize=(14, 10))
 
-    #Length
     axs[0, 0].bar(df["Name"], df["Length"], color='blue')
     axs[0, 0].set_title('Longitud de las Naves')
     axs[0, 0].set_xlabel('Nave')
     axs[0, 0].set_ylabel('Longitud')
+    axs[0, 1].tick_params(axis='x', rotation=90)
 
-    #Cargo
     axs[0, 1].bar(df["Name"], df["Cargo Capacity"], color='red')
     axs[0, 1].set_title('Capacidad de Carga de las Naves')
     axs[0, 1].set_xlabel('Nave')
     axs[0, 1].set_ylabel('Capacidad de Carga por nave')
+    axs[0, 1].tick_params(axis='x', rotation=90)
 
-    # Hyperdrive Rating
     axs[1, 0].bar(df["Name"], df["Hyperdrive Rating"], color='green')
     axs[1, 0].set_title('Clasificación de Hiperimpulsor de las Naves')
     axs[1, 0].set_xlabel('Nave')
     axs[1, 0].set_ylabel('Clasificación de Hiperimpulsor por nave')
+    axs[0, 1].tick_params(axis='x', rotation=90)
 
-    # MGLT
     axs[1, 1].bar(df["Name"], df["MGLT"], color='purple')
     axs[1, 1].set_title('MGLT de las Naves')
     axs[1, 1].set_xlabel('Nave')
     axs[1, 1].set_ylabel('MGLT')
+    axs[0, 1].tick_params(axis='x', rotation=90)
 
     plt.tight_layout()
     plt.show()
